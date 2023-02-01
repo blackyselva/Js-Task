a1ce7a248f72d1770294874c04f09066204fd350
blackyselva <selvam17196@gmail.com>
2023-02-01 22:36:07 +0530

    Adding Task-5.

---
+++ Task-5/task5.js
@@ -0,0 +1,29 @@
+// Task 5 A. larget & smallest
+
+// let numbers = [-1, 4, 9, 0, 16, 24.7];
+
+// var largest = numbers[0];
+// var smallest = numbers[0];
+
+// for (var i = 1; i < numbers.length; i++) {
+
+  // if (numbers[i] > largest) {
+    // largest = numbers[i];
+  // } 
+  // else if (numbers[i] < smallest) {
+    // smallest = numbers[i];
+  // }
+// }
+  // document.write("Largest Number- ", largest);
+  // document.write("<br>");
+  // document.write("<br>");
+  // document.write("Smallest Number- ", smallest);
+  
+  // Task B. Add one number 
+
+// let number= [23,21];
+// let arrayLength= prompt("Enter a Value");
+// number.length= arrayLength;
+// for (i=0; i< arrayLength; i++) {
+	// document.write(number[i]=arrayLength);
+// }
