a10ee589749f66860dc5d007f4465fa4f27d239c
blackyselva <selvam17196@gmail.com>
2023-02-01 22:34:19 +0530

    Adding Task-3.

---
+++ Task-3/task3.js
@@ -0,0 +1,149 @@
+// TASK 3
+
+// 1
+
+// document.write("1. Print N number in sequence loop");
+
+// document.write("<br>");
+// document.write("<br>");
+
+// var n=prompt("Enter a sequence N number");
+// var i=1;
+// while (i<=n){
+	// document.write(i);
+	// document.write("<br>");
+	// i++;
+// }
+
+// document.write("<br>");
+// document.write("<br>");
+// document.write("<br>");
+
+// 2
+
+// document.write("2. Print N number in reverse order loop");
+
+// document.write("<br>");
+// document.write("<br>");
+
+// var n=prompt("Enter a reverse N number");
+// var i=n;
+// while (i>=1) {
+	// document.write(i);
+	// document.write("<br>");
+	// i -=1;
+// }
+
+// document.write("<br>");
+// document.write("<br>");
+// document.write("<br>");
+
+// 3
+
+// document.write("3. Print first N even number loop");
+
+// document.write("<br>");
+// document.write("<br>");
+
+// var n=prompt("Enter a first even N number");
+// var i=0;
+// while (n>0) {
+	// i+=2;
+	// n-=1;
+	// document.write("<br>");
+	
+	// document.write(i);
+// }
+
+// document.write("<br>");
+// document.write("<br>");
+// document.write("<br>");
+
+// 4
+
+// document.write("4. Enter N odd number loop");
+
+// document.write("<br>");
+// document.write("<br>");
+
+// var n=prompt("Enter a odd N number");
+// for (i=0; i<=n; ++i) {
+	// if (i++) {
+	
+	// }
+	// document.write(i);
+	// document.write("<br>");
+// }
+
+// document.write("<br>");
+// document.write("<br>");
+// document.write("<br>");
+
+// 5
+
+// document.write("5. Swap two number using function");
+
+// document.write("<br>");
+// document.write("<br>");
+
+// var a=prompt("Enter a swap first number");
+// var b=prompt("Enter a swap Second number");
+// function swap(x,y) {
+	// document.write(b, " " ,a);
+// }
+// swap(a, b);
+
+// document.write("<br>");
+// document.write("<br>");
+// document.write("<br>");
+
+// 6
+
+// document.write("6. Use function and looping: 1.get name from user 2.get number of times user name will print");
+
+// document.write("<br>");
+// document.write("<br>");
+
+// function printName(name, times) {
+  // for (let i = 0; i < times; i++) {
+    // document.write(name);
+	// document.write("<br>");
+  // }
+// }
+// let userName= prompt("What is your name?");
+// let count= parseInt(prompt("How many time print your name"));
+// printName(userName, count);
+
+// document.write("<br>");
+// document.write("<br>");
+// document.write("<br>");
+
+// 7
+
+// document.write("7. Factorial of a number using for loop");
+
+// document.write("<br>");
+// document.write("<br>");
+
+// var n=(prompt("Enter a factorial number"));
+// var a=1;
+// for (i=n; i>=1; i--) {
+	// a =a*i;
+// }
+// document.write(a);
+
+// document.write("<br>");
+// document.write("<br>");
+// document.write("<br>");
+
+
+// let numbers=[1,2,3,4,5,6,7,8,9,10];
+	// for (i=0; i<numbers.length; i++) {
+		// if (numbers[i] %2== 0) {
+			// document.write("even-");
+		// }
+		// else {
+			// document.write("odd-");
+		// }
+		// document.write(numbers[i], "<br>");
+	// }
